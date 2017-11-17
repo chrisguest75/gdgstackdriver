1628d2b1a58dab1b6c3addefbe91f3d9835ea6c3
Chris Guest <chris.guest@ericsson.com>
2017-11-17 23:26:16 +0000

    Update provisioner

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ For GDG Workshop on building a chatbot for Hutoma using Python, Docker, GCP and 
 * Create a project and add billing to it.  This won't cost $300. 
 * Open a cloud shell in the GCP web console.
 * git clone https://github.com/chrisguest75/gdgstackdriver.git
+* Edit the local cloud-env.sh
+
+
 
 
 TODO:
--- gcloud/provision.sh
+++ gcloud/provision.sh
@@ -6,10 +6,13 @@ sudo apt-get update
 sudo apt-get install     apt-transport-https     ca-certificates     curl     software-properties-common
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
 sudo apt-get install docker-ce
-sudo docker run hello-world
 sudo usermod -aG docker $(whoami)
+exit
 
+#relogin and try the sanity test
+sudo docker run hello-world
 
 
 #stackdriver
@@ -19,7 +22,7 @@ sudo curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
 sudo bash install-logging-agent.sh
 
 
-
+# use gcp logs driver
 sudo systemctl stop docker
 sudo echo '{"log-driver":"gcplogs"}' | sudo tee /etc/docker/daemon.json
 sudo systemctl start docker
